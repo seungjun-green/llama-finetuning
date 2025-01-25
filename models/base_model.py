e823799bb89ce633492e1d819a9efaae5c5bcf92
seungjun-green <lsj3285007@gmail.com>
2025-01-25 19:57:35 +0900

    use torch tokenizer

--- models/base_model.py
+++ models/base_model.py
@@ -7,14 +7,13 @@ from torchtune.models.llama3_2 import llama3_2_1b
 # model_weights_path = "/tmp/Llama-3.2-1B-Instruct/model.safetensors"
 # tokenizer_path = "/tmp/Llama-3.2-1B-Instruct/original/tokenizer.model"
 
-
-def load_base_model(model_weights_path):
-    # load model
-    model = llama3_2_1b()
-    state_dict = load_file(model_weights_path)
-    model.load_state_dict(state_dict, strict=False)
+# def load_base_model(model_weights_path):
+#     # load model
+#     model = llama3_2_1b()
+#     state_dict = load_file(model_weights_path)
+#     model.load_state_dict(state_dict, strict=False)
     
-    return model
+#     return model
 
 def load_model_and_tokenizer(model_weights_path, tokenizer_path):
     # load model
