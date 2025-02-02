0a65af42dcf46044ec5396746609ef260543eb1f
seungjun-green <lsj3285007@gmail.com>
2025-02-03 00:13:58 +0900

    improve comments

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,12 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-def load_base_model(model_name: str):
+def load_base_model(model_name):
+    """load tokenizer and model
+    Args:
+        model_name: model name, huggingface model repo name
+    Returns:
+        tokenizer, base_model
+    """
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     base_model = AutoModelForCausalLM.from_pretrained(model_name)
     return tokenizer, base_model
